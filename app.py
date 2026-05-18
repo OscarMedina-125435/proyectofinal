440dd1b7445b1f8519b5c2ed892530c9d37b7bb3
OscarMedina-125435 <24308060610016@cetis61.edu.mx>
2026-05-17 23:24:01 -0600

    todo

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, redirect, url_for, session
-=======
-from flask import Flask, render_template, request, redirect, url_for,session
->>>>>>> bd136e744ad9921281870fc96ae71e6d3dd0f79c
 from plantas import Plantas 
 
 app = Flask(__name__)
@@ -12,18 +8,11 @@ db_mongo = Plantas()
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
     usuarioo = session.get("usuarioo")
     if not usuarioo:
         return redirect(url_for('login'))
     return render_template('index.html', usuario=usuarioo)
 
-=======
-    user = session.get("usuarioo")
-    if usuarioo:
-        return f"Bienvenido,"
-    return render_template('index.html')
->>>>>>> bd136e744ad9921281870fc96ae71e6d3dd0f79c
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -32,18 +21,11 @@ def register():
         password = request.form.get('password')
         email = request.form.get('email')
         
-<<<<<<< HEAD
         # Guardamos el usuario directamente en MongoDB Atlas
         usuarioo = db_mongo.crear_usuario(nombre, email, password)
         
         if usuarioo:
             # Redirigimos al login para que inicie sesión formalmente
-=======
-
-        usuarioo= db_mongo.crear_usuario(nombre,password,email)
-        
-        if usuarioo:
->>>>>>> bd136e744ad9921281870fc96ae71e6d3dd0f79c
             return redirect(url_for('login'))
         else:
             return "El email ya está registrado en la base de datos."
@@ -61,11 +43,7 @@ def login():
         usuario = db_mongo.verificar_login(email, password)
         
         if usuario:
-<<<<<<< HEAD
             # Guardamos el identificador del usuario en la sesión de Flask
-=======
-            # Si las credenciales son correctas, entramos al index
->>>>>>> bd136e744ad9921281870fc96ae71e6d3dd0f79c
             session['usuarioo'] = usuario['_id']
             print(f"✅ Sesión iniciada para: {usuario['nombre']}")
             return redirect(url_for('index'))
@@ -74,7 +52,6 @@ def login():
 
 
 @app.route("/logout")
-<<<<<<< HEAD
 def logout():
     # Borramos la sesión de forma limpia
     session.pop('usuarioo', None)
@@ -87,12 +64,6 @@ def recuperar_password():
         # Aquí irá tu lógica para buscar el correo en Atlas y enviar el token/correo
         return f"Enlace enviado a {email}"
     return render_template('recuperar.html')
-=======
-
-def logout():
-    session.pop('usuarioo', None)
-    return redirect(url_for('index'))
->>>>>>> bd136e744ad9921281870fc96ae71e6d3dd0f79c
 
 if __name__ == '__main__':
     app.run(debug=True)
